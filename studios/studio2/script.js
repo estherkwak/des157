142716bf00d0e8703c074d737c1732e42c9f508f
estherkwak <ejkwak@ucdavis.edu>
2017-01-26 15:24:14 -0800

    mad lib start

---
+++ studios/studio2/script.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+console.log("reading");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+//capture the sumbit
+  document.form.
+
+});
