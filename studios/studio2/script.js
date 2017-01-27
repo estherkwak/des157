01d0549e965deffb81baad9dab8a4242e96d9807
estherkwak <ejkwak@ucdavis.edu>
2017-01-26 16:01:18 -0800

    studio 2 progress

--- studios/studio2/script.js
+++ studios/studio2/script.js
@@ -6,6 +6,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
 //capture the sumbit
-  document.form.
+  document.f.onsumbit = processForm;
+
+//define process function
+  function processForm() {
+    //store vname in a variable
+    var vname = document.f.vname.value;
+  
+  }
 
 });
